7c84aa407edb5139d785a35fbe69490c76cdb9a7
Asef00 <asefhosseini00@gmail.com>
2025-01-01 15:16:39 +0330

    feat: using promis.all to fetch data paralell

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,26 +9,44 @@ import {
 } from '@/app/lib/data'
 
 export default async function Page() {
-  const revenue = await fetchRevenue()
-  const latestInvoices = await fetchLatestInvoices()
-  const {
-    totalPaidInvoices,
-    totalPendingInvoices,
-    numberOfInvoices,
-    numberOfCustomers,
-  } = await fetchCardData()
+  // const revenue = await fetchRevenue()
+  // const latestInvoices = await fetchLatestInvoices()
+  // const {
+  //   totalPaidInvoices,
+  //   totalPendingInvoices,
+  //   numberOfInvoices,
+  //   numberOfCustomers,
+  // } = await fetchCardData()
+
+  const [latestInvoices, cardData, revenue] = await Promise.all([
+    fetchLatestInvoices(),
+    fetchCardData(),
+    fetchRevenue(),
+  ])
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
+          title="Collected"
+          value={cardData.totalPaidInvoices}
+          type="collected"
+        />
+        <Card
+          title="Pending"
+          value={cardData.totalPendingInvoices}
+          type="pending"
+        />
+        <Card
+          title="Total Invoices"
+          value={cardData.numberOfInvoices}
+          type="invoices"
+        />
         <Card
           title="Total Customers"
-          value={numberOfCustomers}
+          value={cardData.numberOfCustomers}
           type="customers"
         />
       </div>
